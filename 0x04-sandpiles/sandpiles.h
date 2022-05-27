40bf87f8d9606cb7ae42a36c3727d1aff7773398
Julian Mendez <julianlx@hotmail.com>
2022-05-26 20:06:53 -0500

    adding list

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,5 +1,10 @@
-#ifndef SANDPILES
-#define SANDPILES
+#ifndef _SANDPILES_
+#define _SANDPILES_
+#define len 3
+
 #include <stdio.h>
+
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+int ifunstable(int grid1[3][3], int backup[3][3]);
+
 #endif
