bf6d896b60a2590c066e9471570d2198e2d97cdb
Julian Mendez <julianlx@hotmail.com>
2022-07-21 20:18:27 -0500

    Remove return to find and print all solutions before leaving function

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,65 +1,64 @@
 #!/usr/bin/python3
-"""
-    N-queen problem
-    The next algo solve any N queen in any NxN
-    Being N > 3
-"""
-import sys
+"""The N Queens Problem"""
+if __name__ == '__main__':
 
+    import sys
 
-def n_q(t_arr, arr, col, i, n):
-    """
-       n_q - Find all posibles solution for N-queen problem and return it
-             in a list
-       @t_arr: temporaly list to store the all points of a posible solution
-       @arr: store all the solution
-       @col: save a colum use for a queen
-       @i: the row of the chess table
-       @n: Number of queens
-    """
-    if (i > n):
-        arr.append(t_arr[:])
-        return arr
-
-    for j in range(n + 1):
-        if i == 0 or ([i - 1, j - 1] not in t_arr and
-                      [i - 1, j + 1] not in t_arr and
-                      j not in col):
-            if i > 1:
-                dia = 0
-                for k in range(2, i + 1):
-                    if ([i - k, j - k] in t_arr) or ([i - k, j + k] in t_arr):
-                        dia = 1
-                        break
-                if dia:
-                    continue
-            t_arr.append([i, j])
-            col.append(j)
-            n_q(t_arr, arr, col, i + 1, n)
-            col.pop()
-            t_arr.pop()
-
-    return arr
-
-if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        exit(1)
-
+        sys.exit(1)
     try:
-        n = int(sys.argv[1])
-    except:
+        size = int(sys.argv[1])
+    except BaseException:
         print("N must be a number")
-        exit(1)
-
-    if not isinstance(n, int):
-        print("N must be a number")
-        exit(1)
-
-    elif n < 4:
+        sys.exit(1)
+    if size < 4:
         print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
 
-    n_q_arr = n_q([], [], [], 0, n - 1)
-    for i in n_q_arr:
-        print(i)
+    def startSolve():
+        data = [[0 for j in range(size)] for i in range(size)]
+        checkRecursive(data, 0)
+        return
+
+    def checkRecursive(data, c):
+        if (c == size):
+            solution(data)
+            return True
+        ret = False
+        for i in range(size):
+            if (checkPosition(data, i, c)):
+                data[i][c] = 1
+                ret = checkRecursive(data, c + 1) or ret
+                data[i][c] = 0
+        return ret
+
+    def checkPosition(data, r, c):
+        for i in range(c):
+            if (data[r][i]):
+                return False
+        i = r
+        j = c
+        while i >= 0 and j >= 0:
+            if(data[i][j]):
+                return False
+            i = i - 1
+            j = j - 1
+        i = r
+        j = c
+        while j >= 0 and i < size:
+            if(data[i][j]):
+                return False
+            i = i + 1
+            j = j - 1
+        return True
+
+    def solution(data):
+        solve = []
+        for i in range(size):
+            for j in range(size):
+                if(data[i][j] is 1):
+                    solve.append([i, j])
+        print(solve)
+        solve.clear()
+    startSolve()
