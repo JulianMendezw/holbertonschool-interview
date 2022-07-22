7def1376e919605178b8614e354a39334def8942
Julian Mendez <julianlx@hotmail.com>
2022-07-21 20:17:03 -0500

    Remove return to find and print all solutions before leaving function

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,79 +1,65 @@
 #!/usr/bin/python3
 """
-N queens puzzle is the challenge of placing N non-attacking queens
+    N-queen problem
+    The next algo solve any N queen in any NxN
+    Being N > 3
 """
 import sys
 
 
-def printB(brd):
+def n_q(t_arr, arr, col, i, n):
     """
-    print board
+       n_q - Find all posibles solution for N-queen problem and return it
+             in a list
+       @t_arr: temporaly list to store the all points of a posible solution
+       @arr: store all the solution
+       @col: save a colum use for a queen
+       @i: the row of the chess table
+       @n: Number of queens
     """
-    r = []
-    for x in brd:
-        for c in x:
-            if c == 1:
-                r.append([brd.index(x), x.index(c)])
-    print(r)
+    if (i > n):
+        arr.append(t_arr[:])
+        return arr
 
+    for j in range(n + 1):
+        if i == 0 or ([i - 1, j - 1] not in t_arr and
+                      [i - 1, j + 1] not in t_arr and
+                      j not in col):
+            if i > 1:
+                dia = 0
+                for k in range(2, i + 1):
+                    if ([i - k, j - k] in t_arr) or ([i - k, j + k] in t_arr):
+                        dia = 1
+                        break
+                if dia:
+                    continue
+            t_arr.append([i, j])
+            col.append(j)
+            n_q(t_arr, arr, col, i + 1, n)
+            col.pop()
+            t_arr.pop()
 
-def is_safe(brd, row, col, n):
-    """
-    is_safe
-    """
-    for i in range(col):
+    return arr
 
-        if brd[row][i] + brd[row][i + 1] != 0:
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if brd[i][j] == 1:
-            return False
-
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if brd[i][j] == 1:
-            return False
-
-    return True
-
-
-def nqueen(brd, col, n):
-    """
-    nqueen
-    """
-
-    if (col >= n):
-        printB(brd)
-
-    for x in range(n):
-        if is_safe(brd, x, col, n):
-            brd[x][col] = 1
-            if nqueen(brd, col+1, n):
-                return True
-            brd[x][col] = 0
-
-    return False
-
-
-# initial run starting from the col 0!
-def main():
-    """
-    Main
-    """
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         exit(1)
-    if sys.argv[1].isnumeric():
+
+    try:
         n = int(sys.argv[1])
-    else:
+    except:
         print("N must be a number")
         exit(1)
-    if n < 4:
+
+    if not isinstance(n, int):
+        print("N must be a number")
+        exit(1)
+
+    elif n < 4:
         print("N must be at least 4")
         exit(1)
-    brd = [[0 for x in range(n)] for y in range(n)]
-    nqueen(brd, 0, n)
 
-
-if __name__ == '__main__':
-    main()
+    n_q_arr = n_q([], [], [], 0, n - 1)
+    for i in n_q_arr:
+        print(i)
