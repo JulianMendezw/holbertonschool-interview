b1385bcbe2f5c9d6cf0fcb7f55bbcd646c5a00c9
Julian Mendez <julianlx@hotmail.com>
2022-08-18 21:07:46 -0500

    adding new file

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -1,5 +1,4 @@
 #include "binary_trees.h"
-
 /**
  * binary_tree_node - creates a no in binary tree
  * @parent: pointer to the parent node
@@ -7,22 +6,26 @@
  *
  * Return: New Node or NULL on failure
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = NULL;
+binary_tree_t *new_node = NULL;
 
-	new_node = malloc(sizeof(binary_tree_t));
+new_node = malloc(sizeof(binary_tree_t));
 
-	if (!new_node)
-		return (NULL);
-
-	new_node->parent = parent;
-	new_node->left = NULL;
-	new_node->right = NULL;
-	new_node->n = value;
-
-	return (new_node);
+if (!new_node)
+{
+return (NULL);
 }
+
+new_node->parent = parent;
+new_node->left = NULL;
+new_node->right = NULL;
+new_node->n = value;
+
+return (new_node);
+}
+
 /**
  * sort_insert - Function for sort insertion
  * @array: pointer to the first element
@@ -33,29 +36,29 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
  */
 avl_t *sort_insert(int *array, int min, int max)
 {
-	int half;
-	avl_t *tree;
+int half;
+avl_t *tree;
 
-	if (min > max)
-		return (NULL);
+if (min > max)
+return (NULL);
 
-	half = (max + min) / 2;
+half = (max + min) / 2;
 
-	tree = binary_tree_node(NULL, array[half]);
-	if (!tree)
-		return (NULL);
+tree = binary_tree_node(NULL, array[half]);
+if (!tree)
+return (NULL);
 
-	tree->left = sort_insert(array, min, half - 1);
+tree->left = sort_insert(array, min, half - 1);
 
-	tree->right = sort_insert(array, half + 1, max);
+tree->right = sort_insert(array, half + 1, max);
 
-	if (tree->left)
-		tree->left->parent = tree;
+if (tree->left)
+tree->left->parent = tree;
 
-	if (tree->right)
-		tree->right->parent = tree;
+if (tree->right)
+tree->right->parent = tree;
 
-	return (tree);
+return (tree);
 }
 
 /**
@@ -67,12 +70,12 @@ avl_t *sort_insert(int *array, int min, int max)
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-	avl_t *root = NULL;
+avl_t *root = NULL;
 
-	if (!array || size < 1)
-		return (NULL);
+if (!array || size < 1)
+return (NULL);
 
-	root = sort_insert(array, 0, size - 1);
+root = sort_insert(array, 0, size - 1);
 
-	return (root);
+return (root);
 }
