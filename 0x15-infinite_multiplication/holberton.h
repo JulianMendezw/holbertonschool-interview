9d20fb1a2bb6cad0995c5155458e6cb6404ceb43
Julian Mendez <julianlx@hotmail.com>
2022-09-22 18:05:36 -0500

    Fixing holberton

--- 0x15-infinite_multiplication/holberton.h
+++ 0x15-infinite_multiplication/holberton.h
@@ -1,8 +1,10 @@
-#ifndef HOLBERTON
-#define HOLBERTON
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+/* LIBRARIES */
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* PROTOTYPES */
 int _putchar(char c);
-int check_argument(char *argv);
-char *prepare_result(int length);
-void multiplication(char *argv1, char *argv2, int l1, int l2, char *result);
-void insert(char *result, int x, int position);
-#endif
+#endif /* _HOLBERTON_H_ */
